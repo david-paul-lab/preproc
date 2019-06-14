61d3e37c6af9a49d4c8e40d635f7ce078bedca3d
Emma Strawderman <47923713+estrawderman@users.noreply.github.com>
2019-06-14 13:21:27 -0400

    Update sbatch.sh correct version fsl
    version: fsl/6.0.0/b1

--- sbatch.sh
+++ sbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH -t 48:00:00
 #SBATCH --mem=60GB
 #SBATCH --gres=gpu:2 -p gpu
-module load matlab fsl/6.0.1/b1 dcm2niix cuda
+module load matlab fsl/6.0.0/b1 dcm2niix 
 
 # Usage: preproc <dir_filename> <topupflag> <eddyflag> <dtiflag> <bpxflag> <echospacing> <epifactor>
 
@@ -11,4 +11,4 @@ module load matlab fsl/6.0.1/b1 dcm2niix cuda
 #./preproc.sh /scratch/dmi/dpaul2_lab/pitu2/dbdf/scripts/preproc/filenames.txt 1 1 1 0 .66 172
 
 # Run with Bedpostx
-./preproc.sh /scratch/dmi/dpaul2_lab/pitu2/dbdf/scripts/preproc/filenames.txt 0 0 0 1 .66 172
+#./preproc.sh /scratch/dmi/dpaul2_lab/pitu2/dbdf/scripts/preproc/filenames.txt 0 0 0 1 .66 172
